6a6f44c8093b0278088c14cd888038c8b151f3e7
Anand Kumar Singh <anandrkskd@gmail.com>
2022-02-04 13:47:40 +0530

    remove pushd

--- update-with-actions.sh
+++ update-with-actions.sh
@@ -4,14 +4,10 @@ TEMPDIR=$(mktemp -d)
 go run main.go > $TEMPDIR/test-stats.md
 
 
-pushd $TEMPDIR
+cd  $TEMPDIR
 echo -n ${G_TOKEN} | gh auth login --with-token
 git clone git@gist.github.com:102d08389c61e7f7293570bf38205d3b.git
-pushd 102d08389c61e7f7293570bf38205d3b
+cd 102d08389c61e7f7293570bf38205d3b
 mv ../test-stats.md ./test-stats.md
 git commit -m "updated `date`" test-stats.md
 git push origin master
-popd
-popd
-
-rm -rf $TEMPDIR
