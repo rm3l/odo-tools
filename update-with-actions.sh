77003008579ce1498dbe8ad29e49e4c2ea36399e
Anand Kumar Singh <anandrkskd@gmail.com>
2022-02-04 13:44:20 +0530

    update Token

--- update-with-actions.sh
+++ update-with-actions.sh
@@ -5,7 +5,7 @@ go run main.go > $TEMPDIR/test-stats.md
 
 
 pushd $TEMPDIR
-echo -n ${GITHUB_TOKEN} | gh auth login --with-token
+echo -n ${G_TOKEN} | gh auth login --with-token
 git clone git@gist.github.com:102d08389c61e7f7293570bf38205d3b.git
 pushd 102d08389c61e7f7293570bf38205d3b
 mv ../test-stats.md ./test-stats.md
